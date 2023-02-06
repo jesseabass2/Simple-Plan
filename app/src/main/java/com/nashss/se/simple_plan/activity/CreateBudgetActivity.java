fbf4f458b1660b7a43dd8bdf0eba595c95159731
Jesse Bass <jesse.a.bass2@gmail.com>
2023-02-06 08:56:51 -0600

    some stuff

---
+++ app/src/main/java/com/nashss/se/simple_plan/activity/CreateBudgetActivity.java
@@ -0,0 +1,9 @@
+package com.nashss.se.simple_plan.activity;
+
+/**
+ * Instantiates a new CreateBudgetActivity object.
+ *
+ */
+public class CreateBudgetActivity {
+
+}
---
+++ app/src/main/java/com/nashss/se/simple_plan/models/Budget.java
@@ -0,0 +1,42 @@
+package com.nashss.se.simple_plan.models;
+
+public class Budget {
+    private String budgetName;
+    private double monthlyIncome;
+    private double totalExpenses;
+    private double fundsAvailable;
+
+
+    public String getBudgetName() {
+        return budgetName;
+    }
+
+    public void setBudgetName(String budgetName) {
+        this.budgetName = budgetName;
+    }
+
+    public double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public double getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public void setTotalExpenses(double totalExpenses) {
+        this.totalExpenses = totalExpenses;
+    }
+
+    public double getFundsAvailable() {
+        return fundsAvailable;
+    }
+
+    public void setFundsAvailable(double fundsAvailable) {
+        this.fundsAvailable = fundsAvailable;
+    }
+
+}
