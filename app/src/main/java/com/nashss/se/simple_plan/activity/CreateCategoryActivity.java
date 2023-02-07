4be986fa3d2064ab97eac6c89aa8d90d04626e41
Jesse Bass <jesse.a.bass2@gmail.com>
2023-02-07 14:58:16 -0600

    whoops I know I should've created another feature but these are just  skeletons to avoid issues with implementation

---
+++ app/src/main/java/com/nashss/se/simple_plan/activity/CreateCategoryActivity.java
@@ -0,0 +1,4 @@
+package com.nashss.se.simple_plan.activity;
+
+public class CreateCategoryActivity {
+}
---
+++ app/src/main/java/com/nashss/se/simple_plan/results/CreateCategoryResult.java
@@ -0,0 +1,4 @@
+package com.nashss.se.simple_plan.results;
+
+public class CreateCategoryResult {
+}
